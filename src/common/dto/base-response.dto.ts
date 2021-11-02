a000ccfcf2708ffa940b16a3d287fda092e947bd
unknown <pooh0407@ewhain.net>
2021-11-03 05:15:13 +0900

    create :: base-reponse dto

---
+++ src/common/dto/base-response.dto.ts
@@ -0,0 +1,9 @@
+import { ApiProperty } from '@nestjs/swagger';
+
+export class BaseResponseDto {
+  @ApiProperty({ example: 'true' })
+  success: string;
+
+  @ApiProperty({ example: 200 })
+  code: number;
+}
