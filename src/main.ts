78497ab602015ad86e589e32be2a5fdb60d1fb24
unknown <pooh0407@ewhain.net>
2021-11-03 05:08:31 +0900

    create :: httpExeptionFilter

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { HttpExceptionFilter } from './httpException.filter';
 import { setupSwagger } from './swagger/index';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   setupSwagger(app);
+  app.useGlobalFilters(new HttpExceptionFilter());
   await app.listen(3000);
 }
 bootstrap();
