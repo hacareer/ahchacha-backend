d117e5b89e37dc3cbd46982d2b624167f911dac0
unknown <pooh0407@ewhain.net>
2021-11-04 04:25:17 +0900

    create :: strategy

---
+++ src/auth/strategy/jwt.strategy.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { UserService } from './../../user/user.service';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(private readonly userService: UserService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: true,
+      secretOrKey: process.env.JWT_SECRET,
+    });
+  }
+}
