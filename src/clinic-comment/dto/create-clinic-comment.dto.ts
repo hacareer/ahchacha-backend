ee5aeeaf171ffedd5b7bd2f8560b185d0d833b9a
unknown <pooh0407@ewhain.net>
2021-11-07 10:09:55 +0900

    add :: cors 설정

--- src/clinic-comment/dto/create-clinic-comment.dto.ts
+++ src/clinic-comment/dto/create-clinic-comment.dto.ts
@@ -4,7 +4,7 @@ import {ClinicTag} from 'src/constants';
 
 export class CreateClinicCommentDto {
   @ApiProperty({example: '후기 태그 리스트', description: 'T1,T2,T3'})
-  tags: string;
+  contents: string;
 
   @IsNumber()
   @ApiProperty({example: '1', description: '선별진료소 ID'})
--- src/main.ts
+++ src/main.ts
@@ -15,6 +15,7 @@ async function bootstrap() {
       },
     }),
   );
+  app.enableCors();
   app.useGlobalFilters(new HttpExceptionFilter());
   app.useGlobalInterceptors(new TransformInterceptor());
   setupSwagger(app);
