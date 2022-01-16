db7ce13ad8482ee575fc29327738a23262efd961
haron47 <pooh0407@ewhain.net>
2022-01-16 20:47:25 +0900

    refactor: database config 생성

---
+++ src/common/config/database.config.ts
@@ -0,0 +1,11 @@
+import {registerAs} from '@nestjs/config';
+import {TypeOrmModuleOptions} from '@nestjs/typeorm';
+
+export default registerAs(
+  'database',
+  async (): Promise<TypeOrmModuleOptions> => {
+    return {
+      ...require('../../../ormconfig'),
+    };
+  },
+);
