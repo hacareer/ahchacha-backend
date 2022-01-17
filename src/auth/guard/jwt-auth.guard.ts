065b683a3e7f0dea0624bb3a8dc9130faefcc3a4
haron47 <pooh0407@ewhain.net>
2022-01-17 16:40:03 +0900

    feat: jwt guard 에러 핸들링 추가

--- src/auth/guard/jwt-auth.guard.ts
+++ src/auth/guard/jwt-auth.guard.ts
@@ -1,59 +1,29 @@
 import {
-  BadRequestException,
-  ExecutionContext,
-  HttpException,
   Injectable,
+  BadRequestException,
+  ServiceUnavailableException,
 } from '@nestjs/common';
-import {JwtService} from '@nestjs/jwt';
 import {AuthGuard} from '@nestjs/passport';
-import {AuthService} from '../auth.service';
-import {Err} from '../../common/error';
-import {UserService} from './../../user/user.service';
+import {Err} from './../../common/error';
 
 @Injectable()
 export class JwtAuthGuard extends AuthGuard('jwt') {
-  constructor(
-    private authService: AuthService,
-    private userService: UserService,
-  ) {
-    super();
-  }
-  async canActivate(context: ExecutionContext) {
-    const request = context.switchToHttp().getRequest();
-
-    const {authorization} = request.headers;
-    if (authorization === undefined) {
-      throw new BadRequestException(Err.TOKEN.NOT_SEND_TOKEN);
+  handleRequest(err, user, info) {
+    if (user) {
+      return user;
     }
+    switch (info.toString()) {
+      case 'Error: No auth token':
+        throw new BadRequestException(Err.TOKEN.NOT_SEND_TOKEN);
 
-    const token = authorization.replace('Bearer ', '');
-    const tokenValidate = await this.validate(token);
-    request.user = tokenValidate.user ? tokenValidate.user : tokenValidate;
-    return true;
-  }
+      case 'JsonWebTokenError: invalid signature':
+        throw new BadRequestException(Err.TOKEN.INVALID_TOKEN);
 
-  async validate(token: string) {
-    try {
-      // 토큰 검증
-      const tokenVerify = await this.authService.tokenValidate(token);
-      if (tokenVerify.type === 'accessToken') {
-        const user = await this.userService.findUserById(tokenVerify.id);
-        return user;
-      } else {
-        return tokenVerify;
-      }
-    } catch (error) {
-      switch (error.message) {
-        // 토큰에 대한 오류를 판단합니다.
-        case 'invalid token':
-          throw new BadRequestException(Err.TOKEN.INVALID_TOKEN);
+      case 'TokenExpiredError: jwt expired':
+        throw new BadRequestException(Err.TOKEN.JWT_EXPIRED);
 
-        case 'jwt expired':
-          throw new BadRequestException(Err.TOKEN.JWT_EXPIRED);
-
-        default:
-          throw new HttpException('서버 오류입니다.', 500);
-      }
+      default:
+        throw new ServiceUnavailableException(Err.SERVER.UNEXPECTED_ERROR);
     }
   }
 }
